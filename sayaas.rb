52b4319b6a48b78f3f1414002d2446d6949e6a19
Patrick Nielsen <ptnielsen55@gmail.com>
2015-06-10 14:59:09 -0400

    Corrected Server file for default voice and text

--- sayaas.rb
+++ sayaas.rb
@@ -7,11 +7,7 @@ class SaySomethingAPI
   include HTTParty
   base_uri '10.1.10.66:4567'
 
-  message = "Add a voice parameter for a different voice and a text parameter if you'd like for me to say something else."
-
   def speak voice=nil, text=nil
-    voice ||= "Alex"
-    text ||= message
     SaySomethingAPI.get( "/speak", query: { voice: voice, text: text } )
   end
 
--- server.rb
+++ server.rb
@@ -7,11 +7,13 @@ class MyServer < Sinatra::Base
 
   set :bind, "0.0.0.0"
 
+  message = "Add a voice parameter for a different voice and a text parameter if you'd like for me to say something else."
+
   get "/speak" do
-    voice = params[:voice]
-    text = params[:text]
+    voice = params[:voice] || "Alex"
+    text = params[:text] || message
     binding.pry
-    system "say -v '#{voice}' '#{text}'"
+    system "say", "-v", voice, text
   end
 
 end
